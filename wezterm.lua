489f916005c9201b99c0528f826d20e23e7ad8b5
Project <164913866+deadproject@users.noreply.github.com>
2025-01-15 18:13:35 +0300

    Add files via upload

---
+++ wezterm.lua
@@ -0,0 +1,38 @@
+-- wezterm config
+local wezterm = require 'wezterm'
+
+local function format_tab_title(tab, tabs, panes, config, hover, max_width)
+    return "Terminal"
+end
+
+local config = {
+        automatically_reload_config = true,
+        window_close_confirmation = "NeverPrompt",
+        window_decorations = "RESIZE",
+        font_size = 15,
+        window_background_opacity = 0.45,
+        initial_cols = 95,
+        initial_rows = 23,
+        skip_close_confirmation_for_processes_named = { },
+        colors = {
+                foreground = "#FFFFFF",
+                background = "#000000",
+                cursor_bg = "#FFFFFF",
+                cursor_border = "#FFFFFF",
+                cursor_fg = "#000000",
+                selection_bg = "#FFFFFF",
+                selection_fg = "#000000",
+                ansi = {
+                        "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000"
+                },
+                brights = {
+                        "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF"
+                },
+        },
+        cursor_blink_rate = 0,
+}
+
+wezterm.on('format-tab-title', format_tab_title)
+
+return config
+
