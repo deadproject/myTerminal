b6472ad59e26e3fe53407ad50e81d2b3be303c7f
Project <164913866+deadproject@users.noreply.github.com>
2025-11-08 14:33:51 +0300

    Update wezterm.lua

--- wezterm.lua
+++ wezterm.lua
@@ -8,28 +8,30 @@ local config = {
     automatically_reload_config = true,
     window_close_confirmation = "NeverPrompt",
     window_decorations = "RESIZE",
+    enable_tab_bar = false,
     font_size = 15,
     window_background_opacity = 0.5,
-    initial_cols = 95,
-    initial_rows = 24,
+    initial_cols = 100,
+    initial_rows = 25,
     skip_close_confirmation_for_processes_named = {},
     colors = {
-        foreground = "#FFFFFF",
-        background = "#000000",
+        foreground = "#FFFFFF",       -- white text
+        background = "#000000",       -- black background
         cursor_bg = "#FFFFFF",
         cursor_border = "#FFFFFF",
         cursor_fg = "#000000",
         selection_bg = "#FFFFFF",
         selection_fg = "#000000",
         ansi = {
-            "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000",
+            "#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF",
         },
         brights = {
-            "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF",
+            "#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF",
         },
     },
     cursor_blink_rate = 0,
 }
+
 wezterm.on('format-tab-title', format_tab_title)
 
 return config
